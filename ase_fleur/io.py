a84e97c3795f65bd6b5a71d1493a769dd2508ead
Henning Janßen <he.janssen@fz-juelich.de>
2021-09-07 17:20:32 +0200

    Initial try for ase IO plugin for FLEUR

---
+++ ase_fleur/io.py
@@ -0,0 +1,102 @@
+"""Reads Fleur files.
+
+Read structures from inpgen input files or inp.xml files
+
+Built for fleur versions after 0.27
+
+The package masci-tools (version 0.4.11 or greater) is required by this module:
+
+    masci-tools - https://pypi.org/project/masci-tools/
+
+"""
+from ase.atoms import Atoms
+from ase.utils import writer
+from ase.utils.plugins import ExternalIOFormat
+from masci_tools.io.fleur_inpgen import write_inpgen_file, read_inpgen_file
+from masci_tools.io.io_fleurxml import load_inpxml
+from masci_tools.util.xml.xml_getters import get_structure_data
+
+
+inpgen_format = ExternalIOFormat(desc='FLEUR inpgen input file',
+                                 code='1F',
+                                 module='ase_fleur.io',
+                                    magic=[b'*inpgen *', b'*input generator *'],
+                                    glob = 'inp_*')
+
+xml_format =  ExternalIOFormat(desc='FLEUR XML input file',
+                                 code='1F',
+                                 module='ase_fleur.io',
+                                    magic=b'*fleurInputVersion *',)
+
+
+
+def read_fleur_inpgen(fileobj, index=-1):
+    """Reads structure from fleur inpgen file.
+
+    Parameters
+    ----------
+    fileobj: file object
+        File handle from which data should be read.
+
+    Other parameters
+    ----------------
+    index: integer -1
+        Not used in this implementation.
+    """
+
+    # Last parameter is lapw parameters and is not used here
+    cell, atoms, pbc, _ = read_inpgen_file(fileobj, convert_to_angstroem=False)
+    positions, symbols, _ = zip(*atoms)
+
+    return Atoms(symbols=symbols, positions=positions, pbc=pbc, cell=cell)
+
+
+def read_fleur_xml(fileobj, index=-1):
+    """Reads structure from fleur xml file.
+
+    Parameters
+    ----------
+    fileobj: file object
+        File handle from which data should be read.
+
+    Other parameters
+    ----------------
+    index: integer -1
+        Not used in this implementation.
+    """
+
+    xmltree, schema_dict = load_inpxml(fileobj)
+    atoms, cell, pbc = get_structure_data(
+        xmltree, schema_dict, site_namedtuple=True, convert_to_angstroem=False
+    )
+    positions, symbols, _ = zip(*atoms)
+
+    return Atoms(symbols=symbols, positions=positions, pbc=pbc, cell=cell)
+
+
+@writer
+def write_fleur_inpgen(fileobj, atoms, parameters=None, **kwargs):
+    """writes fleur input structure in inpgen input file
+
+    Parameters
+    ----------
+    filename : str
+        Name of file to which data should be written.
+    images : Atom Object or List of Atoms objects
+        This function will write the first Atoms object to file.
+
+    Returns
+    -------
+    """
+
+    atom_sites = [(atom.position, atom.symbol, atom.symbol) for atom in atoms]
+
+    write_inpgen_file(
+        atoms.cell,
+        atom_sites,
+        pbc=atoms.pbc,
+        input_params=parameters,
+        file=fileobj,
+        convert_from_angstroem=False,
+        **kwargs,
+    )
---
+++ setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Materials Virtual Lab
+# Distributed under the terms of the Modified BSD License.
+"""
+setup: usage: pip install -e .
+"""
+import os
+from setuptools import setup, find_packages
+
+
+SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(SETUP_PTH, "README.md")) as f:
+    desc = f.read()
+
+
+setup(
+    name="ase-fleur",
+    packages=find_packages(),
+    version="0.0.1",
+    install_requires=[
+        "ase",
+        "masci-tools>=0.5.0",
+    ],
+    entry_points={'ase.ioformats': ['fleur-inpgen = ase_fleur.io:inpgen_format',
+                                     'fleur-xml= ase_fleur.io:xml_format'
+                                     ]},
+    extras_require={},
+    package_data={},
+    author="Henning Janssen",
+    author_email="he.janssen@fz-juelich.de",
+    maintainer="Henning Janssen",
+    url="https://github.com/janssenhenning/ase-fleur",
+    license="BSD",
+    description="Package adding IO/calculator functionalities for the FLEUR code to the ase package.",
+    long_description=desc,
+    keywords=["ase"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
