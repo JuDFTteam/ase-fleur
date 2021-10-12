f2640dcdaa4e09b14267e29fb90b27d86ccc2259
janssenhenning <henning.janssen@gmx.net>
2021-10-12 19:42:29 +0200

    Fix version retrieval

---
+++ ase_fleur/__init__.py
@@ -0,0 +1,11 @@
+"""
+ASE package for interfacing with the FLEUR code
+"""
+from ase.calculators.calculator import register_calculator_class
+from ase_fleur.calculator import Fleur, FleurProfile
+
+#register calculator class (while there is no entry point in ase)
+register_calculator_class('fleur', Fleur)
+
+__all__ = ('Fleur', 'FleurProfile')
+__version__ = "0.1.0"
