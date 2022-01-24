495fe994beba274086d52dabc89f5151c1900828
janssenhenning <henning.janssen@gmx.net>
2022-01-24 09:47:58 +0100

    Replace ase dependency with explicit git link and modify version to alpha

--- ase_fleur/__init__.py
+++ ase_fleur/__init__.py
@@ -12,5 +12,5 @@ __all__ = ("Fleur", "FleurProfile")
 
 __copyright__ = "Copyright (c), Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany. All rights reserved."
 __license__ = "MIT license, see LICENSE file."
-__version__ = "0.0.1"
+__version__ = "0.0.1a0"
 __authors__ = "The JuDFT team"
