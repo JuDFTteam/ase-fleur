74053bc3b782681eb805de2eccd1017288cf06e0
janssenhenning <henning.janssen@gmx.net>
2022-06-24 10:49:37 +0200

    Update test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,41 @@
 Test configuration
 """
 import ase_fleur  # Make sure the calculator class is registered
+from ase.test.factories import Factories
+
+import pytest
 
 version = ase_fleur.__version__
+
+
+def pytest_addoption(parser):
+    """
+    Add cmdline options
+    """
+    parser.addoption("--calculator", action="store_true", help="Run the calculator tests")
+
+
+def pytest_configure(config):
+    """
+    Add markers
+    """
+    config.addinivalue_line("markers", "calculator: test running the FleurCalculator")
+
+
+def pytest_collection_modifyitems(session, config, items):
+    """
+    Deselect the calculator tests if not specified
+    """
+    if not config.getoption("--calculator"):
+        for item in items.copy():
+            if "calculator" in item.keywords:
+                items.remove(item)
+
+        config.hook.pytest_deselected(items=items)
+
+
+@pytest.fixture(scope="session")
+def generate_factories(pytestconfig):
+    if pytestconfig.getoption("--calculator"):
+        return Factories(["fleur"])
+    return None
